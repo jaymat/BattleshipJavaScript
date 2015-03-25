bbac2bc17146e3aa0f9f4a8951d0c4e5d3aea1e2
jaymat <jaymat@users.noreply.github.com>
2015-03-24 18:15:46 -0700

    Update Battleship.js
    This is a fork of Liz's Battleship.js. It includes a simple single array and places our random number into it at the index that matches its value (ie: Random number = 5, placed at index 5) It then prints this array in an Alert box using .join() to separate the elements.

--- Battleship.js
+++ Battleship.js
@@ -1,18 +1,32 @@
+
+//create an array of 5 elements and assign them each a letter (type string) value
+var myArray = ['a','b','c','d','e'];
+//var myArray = new Array(5); //don't forget the ';' at the end!
+
 // this function generates a random number between a specified minimum and maximum number
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-// for our game, we'll pick a number from 1 to 5. This is where our "battleship" is.
-var correctAnswer = getRandomNumber(1,5);
+//For our game, we'll pick a number from 0 to 4. This is where our "battleship" is.
+var correctAnswer = getRandomNumber(0,4);
 
-//start the game:
-var userInput = parseInt(prompt("Let's play Battleship! To fire at a battleship, please enter another number from 1 to 5:"), 10);
+//Let's assign this 'correctAnswer' random number to our array in the same 'index' (place holder) as the value of this number, like so:
+myArray[correctAnswer] = correctAnswer;
 
-// the game loop: keep asking the player to guess until they guess the right answer
+//Remember, the array has a length of 5 but it starts with index 0; therefore, the 5th (last) element in our array is at index 4. **You will typically reduce by '-1' to compensate for the difference between array.length and array.index**
+
+//Let's print our array so we can see what the random number is and it was placed
+alert(myArray.join(', '));
+// NOTE: .join() is a method that concatinates all elements in an array. It takes an 'delimiter' value - in this case a comma and space - to place between them.
+
+//START THE GAME:
+var userInput = parseInt(prompt("Let's play Battleship! To fire at a battleship, please enter another number from 0 to 4:"), 10);
+
+//The game loop: keep asking the player to guess until they guess the right answer
 while (userInput !== correctAnswer) {
-  var userInput = parseInt(prompt("You missed! To fire again, please enter another number from 1 to 5:"), 10);
+  var userInput = parseInt(prompt("You missed! To fire again, please enter another number from 0 to 4:"), 10);
 }
 
-// the code below will only run when the game loop ends:
+//The code below will only run when the game loop ends:
 alert("You win! Battleship sunk!")
